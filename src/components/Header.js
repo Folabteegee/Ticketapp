71047622a2712cf2cad7b7a348e8d311b8d26234
Taiwo <taiwoglory136@gmail.com>
2025-10-25 16:00:24 +0100

    addedw

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
       <div className="container">
         <div className="header-content">
           <Link to="/" className="nav-linki">
-            TicketFlow
+            TicketFloww
           </Link>
 
           {/* Desktop Navigation */}
